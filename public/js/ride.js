e90868c5f5f0b5f0a4573c38b0cbf366d324b3c2
Philip Riley <hiriley510@gmail.com>
2015-01-06 18:50:04 -0800

    fixes add to join ride 7

--- public/js/ride.js
+++ public/js/ride.js
@@ -20,7 +20,7 @@ $( document ).ready(function() {
             $('#none').replaceWith("<li value="+response['first_name'] + "_" + response['last_name']+"><a href='/user/"+response['id']+"''>" +response['first_name'] + " " + response['last_name']+"</a></li>")
             $('#join').on('click', function(event){event.preventDefault()})
             $('#join').text("You've joined this ride!")
-          } else if (response['first_name'] !== undefined || "undefined"){
+          } else if (!$('#attendees').find('li[value='+response['first_name'] + "_" + response['last_name']+']').length > 0) {
             $('#attendees:last').append("<ul><li value="+response['first_name'] + "_" + response['last_name']+"><a href='/user/"+response['id']+"''>" +response['first_name'] + " " + response['last_name']+"</a></li></ul>")
             $('#join').on('click', function(event){event.preventDefault()})
             $('#join').text("You've joined this ride!")
